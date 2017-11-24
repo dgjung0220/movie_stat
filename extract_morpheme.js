58d79cd32550fbfdeeaa26c50e783e81720a1a12
Donggoo <dgjung0220@gmail.com>
2017-11-24 14:22:56 +0900

    extract_morpheme

---
+++ extract_morpheme.js
@@ -0,0 +1,29 @@
+var mecab = require('mecab-ya');
+var fs = require('fs');
+var xlsx = require('exceljs');
+var xl = require('excel4node');
+
+var workbook = new xlsx.Workbook();
+workbook.xlsx.readFile('test.xlsx')
+    .then(function() {
+        var worksheet = workbook.getWorksheet('total');
+        var m = new Map();
+        var temp;
+
+        fs.exists('morpheme.txt', (exists) => {
+            if (exists) {
+                fs.unlinkSync('morpheme.txt');
+            }
+        })
+        
+        worksheet.eachRow({ includeEmpty: true }, function(row, rowNumber) {
+            if (rowNumber > 1 && row.values[2] > 8) {   
+                mecab.nouns(row.values[4], function (err, result) {
+                    for(var i in result) {
+                        fs.appendFileSync('morpheme.txt', result[i]+'\n');
+                    }
+                });
+            }    
+        });
+    }
+)
